ec3043d5f5b2230b96a49cfd5d1997c31e3ea616
naryzhnaj <naryzhnaj@gmail.com>
2018-07-01 00:48:30 +0300

    разбито на модули

---
+++ js/card.js
@@ -0,0 +1,56 @@
+'use strict';
+(function () {
+  var createOneOffer = function (house) {
+
+    var offer = house.offer;
+    var houseType;
+    switch (offer.type) {
+      case 'flat':
+        houseType = 'Квартира';
+        break;
+      case 'bungalo':
+        houseType = 'Бунгало';
+        break;
+      case 'house':
+        houseType = 'Дом';
+        break;
+      case 'palace':
+        houseType = 'Дворец';
+        break;
+    }
+
+    var offerTemplate = document.querySelector('template').content;
+    var newOffer = offerTemplate.cloneNode(true);
+    newOffer.querySelector('.popup__title').textContent = offer.title;
+    newOffer.querySelector('.popup__text--address').textContent = offer.address;
+    newOffer.querySelector('.popup__avatar').src = house.author.avatar;
+    newOffer.querySelector('.popup__text--price').textContent = offer.price + String.fromCharCode(0x20bd) + '/ночь';
+    newOffer.querySelector('.popup__type').textContent = houseType;
+    newOffer.querySelector('.popup__text--capacity').textContent = offer.rooms + ' комнаты для ' + offer.guests + ' гостей.';
+    newOffer.querySelector('.popup__text--time').textContent = 'Заезд после' + offer.checkin + ', выезд до ' + offer.checkout;
+    newOffer.querySelector('.popup__description').textContent = offer.description;
+
+    var housefeatures = offer.features;
+    var allFeatures = newOffer.querySelector('.popup__features').children;
+    for (var i = 0; i < housefeatures.length; i++) {
+      allFeatures[i].className = 'popup__feature popup__feature--' + housefeatures[i];
+    }
+
+    var housePhotos = offer.photos;
+    var photos = newOffer.querySelector('.popup__photos');
+    photos.firstElementChild.src = housePhotos[0];
+    for (i = 1; i < housePhotos.length; i++) {
+      var newPicture = photos.firstElementChild.cloneNode(true);
+      newPicture.src = housePhotos[i];
+      photos.appendChild(newPicture);
+    }
+    return newOffer;
+  };
+
+  window.insertOffer = function (currentOffer) {
+    var fragment = document.createDocumentFragment();
+    var newCard = createOneOffer(currentOffer);
+    fragment.appendChild(newCard);
+    document.querySelector('.map').insertBefore(fragment, document.querySelector('.map__filters-container'));
+  };
+})();
--- js/form.js
+++ js/form.js
@@ -1,14 +1,37 @@
 'use strict';
 (function () {
+  var X0 = 570;
+  var Y0 = 375;
   var MIN_GUESTS = 0;
 
+  var map = document.querySelector('.map');
   var adForm = document.querySelector('.ad-form');
+  var fieldsets = adForm.getElementsByTagName('fieldset');
   var rooms = document.getElementById('room_number');
   var capacity = document.getElementById('capacity');
   var timein = document.getElementById('timein');
   var timeout = document.getElementById('timeout');
   var houseType = document.getElementById('type');
   var price = document.getElementById('price');
+  var success = document.querySelector('.success');
+  var mainPin = document.querySelector('.map__pin--main');
+
+  window.initialStage = function (isDisabled) {
+    if (isDisabled) {
+      adForm.classList.add('ad-form--disabled');
+      map.classList.add('.map--faded');
+      document.getElementById('address').value = X0 + ', ' + Y0;
+      mainPin.style.top = Y0 + 'px';
+      mainPin.style.left = X0 + 'px';
+    } else {
+      adForm.classList.remove('ad-form--disabled');
+      map.classList.remove('.map--faded');
+    }
+
+    for (var i = 0; i < fieldsets.length; i++) {
+      fieldsets[i].disabled = isDisabled;
+    }
+  };
 
   window.openForm = function () {
     // синхронизация цены и типа жилья
@@ -28,7 +51,7 @@
           break;
       }
     });
-    
+
     // синхронизация заезда
     timein.addEventListener('change', function () {
       timeout.selectedIndex = timein.selectedIndex;
@@ -48,8 +71,19 @@
         rooms.setCustomValidity('Извините, число комнат должно соответствовать числу гостей');
       } else {
         rooms.setCustomValidity('');
-        adForm.querySelector('.success').classList.remove('.hidden');
+
+        success.classList.remove('hidden');
+        window.initialStage(true);
+
+        document.addEventListener('click', function () {
+          success.classList.add('hidden');
+        });
       }
     });
+
+    adForm.addEventListener('reset', function () {
+      window.initialStage(true);
+    });
+
   };
 })();
